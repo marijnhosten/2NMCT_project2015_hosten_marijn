ba3068360eab655323d810c0f4bd881aaed4e533
theonlymarijn <marijn.hosten@student.howest.be>
2015-04-28 11:27:07 +0200

    Initial commit

---
+++ app/src/main/java/mobile/nmct/desopdracht/OverzichtFragment.java
@@ -0,0 +1,11 @@
+package mobile.nmct.desopdracht;
+
+import android.app.ListFragment;
+
+/**
+ * Created by Heimerdinger on 27/04/2015.
+ */
+public class OverzichtFragment extends ListFragment{
+
+
+}
